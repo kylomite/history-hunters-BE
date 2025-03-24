5e3c38291d24597de92a8acf10efb0f3a4f08591
kylomite <kdelaney99@gmail.com>
2025-03-24 14:10:38 -0400

    add question migration

---
+++ internal/migrations/create_questions_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE questions (
+    id SERIAL PRIMARY KEY,
+    player_session_id INT NOT NULL,
+    text VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_player_session
+        FOREIGN KEY(player_session_id) 
+        REFERENCES player_sessions(id) 
+        ON DELETE CASCADE
+);
