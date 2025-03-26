56e73b65277db8b3cfdbb64c654340c6e76563d8
kylomite <kdelaney99@gmail.com>
2025-03-25 21:21:23 -0400

    pass failing FindByID test for answer

--- internal/models/answer/answer.go
+++ internal/models/answer/answer.go
@@ -48,6 +48,27 @@ func (a *Answer) Save(db *sql.DB) error {
 	return nil
 }
 
+func FindByID(db *sql.DB, answerID int) (*Answer, error) {
+	query := `
+		SELECT id, question_id, answer_text, correct, created_at, updated_at
+		FROM answers
+		WHERE id = $1
+	`
+
+	row := db.QueryRow(query, answerID)
+
+	a := &Answer{}
+	err := row.Scan(&a.ID, &a.QuestionID, &a.AnswerText, &a.Correct, &a.CreatedAt, &a.UpdatedAt)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, errors.New("answer not found")
+		}
+		return nil, err
+	}
+
+	return a, nil
+}
+
 func (a *Answer) Delete(db *sql.DB) error {
 	query := `DELETE FROM answers WHERE id = $1`
 	_, err := db.Exec(query, a.ID)
